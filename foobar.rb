7d417a6776382131ba34f0f801578b5cf1f849d4
Joe Logan <jlogan@berkeley.edu>
2017-09-18 13:23:32 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,24 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    i = 0
+    z = []
+    while i < a.length
+    	currnum = a[i].to_i
+    	if (currnum % 2) == 0 and currnum < 10
+    		z << currnum+2
+    	end
+    	i += 1
+    end
+    z = z.uniq
+    puts z
+    sum = 0
+    k = 0
+    while k < z.length
+    	sum += z[k]
+    	k += 1
+    end
+    return sum
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,23 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  z = (a+b)**2
+  return z
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a = a.sort
+  i = 0
+  while i < a.length
+  	a[i] += 1
+  	i += 1
+  end
+  return a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
